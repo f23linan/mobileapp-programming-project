08dec34836e56243617ee7a2e4db3cb056803656
f23linan <f23linan@student.his.se>
2024-05-20 10:02:54 +0200

    Dog class populated

--- app/src/main/java/com/example/project/Dogs.java
+++ app/src/main/java/com/example/project/Dogs.java
@@ -1,4 +1,43 @@
-package com.example.project;
+package com.example.networking;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
 
 public class Dogs {
+
+    public String ID;
+    @SerializedName("name")
+    public String name;
+
+    public String location;
+    public String size;
+    public String cost;
+    public Dogs() {
+        name = "Breed name:";
+        location = "Originated:";
+        size = "Aprox weight:";
+        cost = "Aprox cost:";
+    }
+
+    public Dogs(String name) {
+        this.name = name;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return name;
+        return location;
+        return size;
+        return cost;
+    }
+
+    public String getTitle() {
+        return name;
+        return location;
+        return size;
+        return cost;
+    }
+
 }
