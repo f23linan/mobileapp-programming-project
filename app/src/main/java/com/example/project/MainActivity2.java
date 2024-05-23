6e7712fa9a913b295a550dc95526022333779e31
F23LINAN <F23linan@student.his.se>
2024-05-23 14:17:08 +0200

    final updates of java files

--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -11,14 +11,5 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
-        TextView textView = findViewById(R.id.text);
-
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String name = extras.getString("name");
-            int number = extras.getInt("number");
-            textView.setText(name);
-        }
     }
 }
